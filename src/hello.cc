88078c9358ae20530bcd2649d2bdd461c087e570
tony62olsson <tony.r.olsson@gmail.com>
2019-02-13 11:50:15 +0000

    Template test

--- src/hello.cc
+++ src/hello.cc
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <type_traits>
 
-static const char hello[] = "hello world!"
-;
+
+template<typename T>
+T obj();
+
+template<typename T>
+auto len(T const& t)  -> decltype((void)t.size(),
+                                  (void)typename std::enable_if<std::is_same<T,std::vector<int>>::value>::type(),
+                                  typename T::size_type())
+{
+    return t.size();
+}
+
+std::size_t len(...) {
+    return 0;
+}
+
 int main()
 {
-    std::cout << hello << std::endl;
+    int a[10];
+    std::cout << len(a) << std::endl;
+    std::vector<int> v;
+    v.push_back(10);
+    std::cout << len(v) << std::endl;
+    std::allocator<int> x;
+    std::cout << len(x) << std::endl;
 }
